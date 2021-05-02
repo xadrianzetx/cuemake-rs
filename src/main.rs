e6c558d48e1ff099f5833399598d2fc8d7b2270a
Adrian Zuber <xadrianzetx@gmail.com>
2021-05-02 12:15:48 +0200

    add CLI help

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,21 @@ use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("cuemake")
+        .version("0.1.0")
+        .author("xadrianzetx")
+        .about(".cue file generator for .bin or .BIN PSX ROMs")
         .args(&[
             Arg::with_name("binary")
                 .short("b")
                 .long("binary")
                 .takes_value(true)
-                .required(true),
+                .required(true)
+                .help("Path to .bin or .BIN PSX ROM"),
             Arg::with_name("output")
                 .short("o")
                 .long("output")
                 .takes_value(true)
+                .help("Path to save generated .cue file. Saves into current dir if not specified")
         ])
         .get_matches();
     
